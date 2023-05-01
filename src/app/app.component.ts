7cf1ae8aa89cfd20330b156b94746479f68bad73
katlp48 <130487303+katlp48@users.noreply.github.com>
2023-05-01 11:39:01 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import {  NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demo';
   open:boolean = true
 
-  constructor(private router:Router)
+  constructor(private router:Router,private http: HttpClient)
   {
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
@@ -24,6 +25,8 @@ export class AppComponent {
       }
     })
 
+  }
+  ngOnInit():void{
   }
 }
 
