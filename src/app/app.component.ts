5d9f678ccf8c947a00ec6225142613bc136b00c9
katlp48 <130487303+katlp48@users.noreply.github.com>
2023-05-01 11:41:29 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {  NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
