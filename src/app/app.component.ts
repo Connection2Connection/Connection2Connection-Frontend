a94e863397d7728f366700158d2082769c806364
Portinho45 <xtrevator@gmail.com>
2023-06-20 13:27:16 -0500

    Security_2023-06-20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,14 +22,30 @@ export class AppComponent implements OnInit {
     this.role=this.loginService.showRole();
     return this.loginService.verificar();
   }
-  validarRol(){
-    if(this.role=='ADMIN' || this.role=='USER'){
+  //validarRolAdmin: si es admin lo puede ver
+  validarRolAdmin(){
+    if(this.role=='ADMIN'){
       return true;
     }else{
       return false;
     }
   }
-
+  //validarRolEstudiante: si no es estudiante lo puede ver
+  validarRolEstudiante(){
+    if(this.role!=='ESTUDIANTE'){
+      return true;
+    }else{
+      return false;
+    }
+  }
+  //validarRolReclutador: si no es reclutador lo puede ver
+  validarRolReclutador(){
+    if(this.role!=='RECLUTADOR'){
+      return true;
+    }else{
+      return false;
+    }
+  }
   constructor(private router:Router,private http: HttpClient, private loginService: LoginService)
   {
     this.router.events.pipe(
@@ -45,6 +61,8 @@ export class AppComponent implements OnInit {
 
   }
   ngOnInit():void{
+        this.role=this.loginService.showRole();
+    console.log(this.role);
   }
 }
 
--- src/app/components/pages.module.ts
+++ src/app/components/pages.module.ts
@@ -2,18 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { MatTableModule } from '@angular/material/table'
+import { MatTableModule } from '@angular/material/table';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input'
-import { MatDatepickerModule } from '@angular/material/datepicker'
-import { MatNativeDateModule } from '@angular/material/core'
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { PagesRoutingModule } from './pages-routing.module';
@@ -112,9 +110,7 @@ import { RolCreaeditaComponent } from './rol/rol-creaedita/rol-creaedita.compone
   ],
   imports: [
     CommonModule,
-    BrowserModule,
     HttpClientModule,
-    BrowserAnimationsModule,
     MatTableModule,
     ReactiveFormsModule,
     FormsModule,
