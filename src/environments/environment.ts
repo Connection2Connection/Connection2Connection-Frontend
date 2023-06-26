c4c0604aac4f7356e3e693b6e291f117f5d62962
Portinho45 <xtrevator@gmail.com>
2023-06-25 20:29:53 -0500

    deploy frontend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment={
   production:false,
-  base:"http://localhost:8080"
+  base:"https://connection2connection-backend.onrender.com"
   //base:"https://data-connection2connection.onrender.com"
 }
